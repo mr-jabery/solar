54fa641f419250627167bcfa9787977e4b768069
mohammad <picobaz@yahoo.com>
2023-12-19 15:53:08 +0330

    initial commit

---
+++ src/SolarDate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Meyti\SolarDate;
+use Illuminate\Support\Facades\Facade;
+
+class SolarDate extends Facade
+{
+
+    public function test()
+    {
+        return "Hello world!!!";
+    }
+}
